713da0a8a3b33bb05f7f637d6778fced905461ab
Jami Gibbs <jami0821@gmail.com>
2018-04-24 12:01:42 -0500

    Through bonus 1

--- bash.js
+++ bash.js
@@ -1,15 +1,20 @@
-process.stdout.write('promt > ')
+let pwd = require('./pwd').getPwd;
+let ls = require('./ls');
+let cat = require('./cat');
+
+process.stdout.write('promt > ');
 
 process.stdin.on('data', (data) => {
-
     const cmd = data.toString().trim();
 
-    if (cmd === 'pwd') {
-        process.stdout.write(process.cwd())
-    } else {
-        process.stdout.write('You typed: ' + cmd);
-        process.stdout.write('\nprompt > ');
-    }
-    
+    console.log('command', cmd.slice(0, 3));
+    console.log('file', cmd.slice(4));
 
+    if (cmd === 'pwd') {
+        pwd(cmd);
+    } else if (cmd === 'ls'){
+        ls();
+    } else if (cmd.slice(0, 3) === 'cat'){
+        cat(cmd.slice(4));
+    }
 })
---
+++ pwd.js
@@ -0,0 +1,7 @@
+module.exports.getPwd = function(data){
+  process.stdout.write(process.cwd())
+  // process.stdout.write('\nYou typed: ' + data);
+  // process.stdout.write('\nprompt > ');
+}
+
+
